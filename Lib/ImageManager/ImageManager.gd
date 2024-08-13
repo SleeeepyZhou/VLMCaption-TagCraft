09eba64befd033fd44d8de363abdc6a3d1001884
SleeeepyZhou <sleeeepyzhou@yeah.net>
2024-08-13 19:49:20 +0800

    Clean code, Optimize experience, New functional UI planning.

--- Lib/ImageManager/ImageManager.gd
+++ Lib/ImageManager/ImageManager.gd
@@ -1,7 +1,26 @@
-extends MarginContainer
+extends HSplitContainer
 
 func _on_thank_meta_clicked(meta):
 	OS.shell_open(meta)
 
-func _ready():
+func _on_path_text_changed(new_text):
 	pass # Replace with function body.
+
+func _on_enter_pressed():
+	pass # Replace with function body.
+
+func _on_show_mod_item_selected(index):
+	pass # Replace with function body.
+
+func _on_backpath_text_changed(new_text):
+	pass # Replace with function body.
+
+func _on_set_color_color_changed(color):
+	pass # Replace with function body.
+
+func _on_set_back_button_up():
+	$FileShow/Backpic
+	$FileShow/Background
+
+const v_unit = "res://Lib/ImageManager/image_v_unit.tscn"
+const h_unit = "res://Lib/ImageManager/image_#_unit.tscn"
