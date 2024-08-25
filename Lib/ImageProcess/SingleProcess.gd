53815ddd8fdf4e360ed0697481190f341c641ff2
SleeeepyZhou <sleeeepyzhou@yeah.net>
2024-08-26 01:10:34 +0800

    2024.8.25

--- Lib/ImageProcess/SingleProcess.gd
+++ Lib/ImageProcess/SingleProcess.gd
@@ -18,3 +18,12 @@ func _on_caption_button_up():
 	$SingleImage/UpOut/Output.text = "Processing..."
 	$SingleImage/UpOut/Output.text = await %ApiUtils.run_api(single_path)
 	Global.is_run = false
+
+func _on_line_edit_text_submitted(new_text):
+	var path : String = new_text
+	if !path.get_extension().is_empty() and Global.IMAGE_TYPE.has(path.get_extension().to_upper()) and visible:
+		var image = Image.load_from_file(path)
+		$SingleImage/UpOut/ImageUp/LineEdit.position.y = 0
+		$SingleImage/UpOut/ImageUp/Label.visible = false
+		$SingleImage/UpOut/ImageUp.texture = ImageTexture.create_from_image(image)
+		single_path = path
