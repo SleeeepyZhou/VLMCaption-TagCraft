28217336495ba197c1e6bb4aa3e85fdb12a4c017
SleeeepyZhou <sleeeepyzhou@yeah.net>
2024-10-01 23:35:02 +0800

    New func fram

--- Lib/GlobalLib.gd
+++ Lib/GlobalLib.gd
@@ -1,5 +1,27 @@
 extends Node
 
+const stopwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", 
+			"your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", 
+			"she", "her", "hers", "herself", "it", "its", "itself", "they", "them", 
+			"their", "theirs", "themselves", "what", "which", "who", "whom", "this", 
+			"that", "these", "those", "am", "is", "are", "was", "were", "be", "been", 
+			"being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", 
+			"an", "the", "and", "but", "if", "or", "because", "as", "until", "while", 
+			"of", "at", "by", "for", "with", "about", "against", "between", "into", 
+			"through", "during", "before", "after", "above", "below", "to", "from", 
+			"up", "down", "in", "out", "on", "off", "over", "under", "again", "further", 
+			"then", "once", "here", "there", "when", "where", "why", "how", "all", 
+			"any", "both", "each", "few", "more", "most", "other", "some", "such", 
+			"no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", 
+			"s", "t", "can", "will", "just", "don", "should", "now"]
+const regex_word = "\b(i|me|my|myself|we|our|ours|ourselves|you|your|yours|yourself|yourselves|
+			he|him|his|himself|she|her|hers|herself|it|its|itself|they|them|their|theirs|themselves|
+			what|which|who|whom|this|that|these|those|am|is|are|was|were|be|been|being|have|has|had|
+			having|do|does|did|doing|a|an|the|and|but|if|or|because|as|until|while|of|at|by|for|with|
+			about|against|between|into|through|during|before|after|above|below|to|from|up|down|
+			in|out|on|off|over|under|again|further|then|once|here|there|when|where|why|how|all|
+			any|both|each|few|more|most|other|some|such|no|nor|not|only|own|same|so|than|too|very|
+			s|t|can|will|just|don|should|now)\b"
 # 线程池
 var maxhttp : int = 10
 var is_run : bool = false
