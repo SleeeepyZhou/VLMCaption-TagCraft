a8eb1edf70e4ca3b846b48f78ff2b1d63a64fefb
SleeeepyZhou <sleeeepyzhou@yeah.net>
2024-11-17 01:12:03 +0800

    New

---
+++ Lib/TagManager/TagAnalysis.gd
@@ -0,0 +1,8 @@
+extends VBoxContainer
+
+@onready var path1: LineEdit = $Input1/Path
+@onready var path2: LineEdit = $Input2/Path
+@onready var outpath: LineEdit = $Input3/Path
+
+func _ready() -> void:
+	pass # Replace with function body.
